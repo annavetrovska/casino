fda25996530678b64bd36d060c2ed104e98be6e3
annavetrovska <vetrovskaanna6@gmail.com>
2026-05-27 22:04:09 +0200

    refactor: consolidate blackjack game logic into script.js; remove script_blackjack.js

--- script.js
+++ script.js
@@ -120,3 +120,194 @@ function flip(chosenSide) {
 
     updateUI();
 }
+
+//BLACKJACK GAME CODE
+
+let deck = [];
+let playerHand = [];
+let dealerHand = [];
+let gameActive = false;
+let totalPoints = 0;
+
+// Začátečnické vytvoření balíčku pomocí klasických cyklů
+function createDeck() {
+    let newDeck = [];
+    // 4 barvy
+    for (let i = 0; i < 4; i++) {
+        // Karty od 1 do 10
+        for (let value = 1; value <= 10; value++) {
+            newDeck.push(value);
+        }
+    }
+    
+    // Klasické zamíchání balíčku
+    for (let i = newDeck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let docasnaKarta = newDeck[i];
+        newDeck[i] = newDeck[j];
+        newDeck[j] = docasnaKarta;
+    }
+    return newDeck;
+}
+
+// Spočítání skóre pomocí obyčejného for cyklu
+function calculateScore(hand) {
+    let celkem = 0;
+    for (let i = 0; i < hand.length; i++) {
+        celkem = celkem + hand[i];
+    }
+    return celkem;
+}
+
+// Aktualizace bodů na obrazovce
+function updatePoints(amount) {
+    totalPoints = totalPoints + amount;
+    if (totalPoints < 0) {
+        totalPoints = 0; 
+    }
+    const scoreElement = document.getElementById('score-counter');
+    if (scoreElement) {
+        scoreElement.innerText = 'Celkové body: ' + totalPoints;
+    }
+}
+
+// Vykreslení karet bez použití složitého .map() a .join()
+function renderHand(hand, hideFirstCard) {
+    let htmlKodu = "";
+    for (let i = 0; i < hand.length; i++) {
+        if (hideFirstCard === true && i === 0) {
+            // Skrytá karta dealera
+            htmlKodu = htmlKodu + '<div class="card-visual" style="background:#555555; color:white;">?</div>';
+        } else {
+            // Normální karta
+            htmlKodu = htmlKodu + '<div class="card-visual">' + hand[i] + '</div>';
+        }
+    }
+    return htmlKodu;
+}
+
+function startGame() {
+    deck = createDeck();
+    // Každý dostane na začátku jednu kartu
+    playerHand = [deck.pop()];
+    dealerHand = [deck.pop()];
+    gameActive = true;
+
+    const startControls = document.getElementById('start-controls');
+    if (startControls) startControls.style.display = 'none';
+
+    const gameControls = document.getElementById('game-controls');
+    if (gameControls) gameControls.style.display = 'flex';
+
+    const messageElement = document.getElementById('message');
+    if (messageElement) {
+        messageElement.innerText = "Tvoje řada! Chceš další kartu nebo stát?";
+    }
+
+    updateBlackjackUI(true);
+}
+
+function updateBlackjackUI(hideDealer) {
+    const playerCards = document.getElementById('player-cards');
+    if (playerCards) {
+        playerCards.innerHTML = renderHand(playerHand, false);
+    }
+    const playerScore = document.getElementById('player-score');
+    if (playerScore) {
+        playerScore.innerText = 'Skóre: ' + calculateScore(playerHand);
+    }
+
+    const dealerCards = document.getElementById('dealer-cards');
+    const dealerScore = document.getElementById('dealer-score');
+
+    if (hideDealer === true) {
+        if (dealerCards) dealerCards.innerHTML = renderHand(dealerHand, true);
+        if (dealerScore) dealerScore.innerText = 'Skóre: ?';
+    } else {
+        if (dealerCards) dealerCards.innerHTML = renderHand(dealerHand, false);
+        if (dealerScore) dealerScore.innerText = 'Skóre: ' + calculateScore(dealerHand);
+    }
+}
+
+function hit() {
+    if (gameActive === false) {
+        return;
+    }
+
+    playerHand.push(deck.pop());
+    let pScore = calculateScore(playerHand);
+
+    if (pScore > 21) {
+        updateBlackjackUI(false);
+        updatePoints(-5); // Ztráta bodů za přetažení
+        endGame('Přetáhl jsi! Máš ' + pScore + '. Dealer vyhrává! 😢');
+    } else if (pScore === 21) {
+        updateBlackjackUI(true);
+        stand(); // Při 21 automaticky stojíš
+    } else {
+        updateBlackjackUI(true);
+    }
+}
+
+function stand() {
+    if (gameActive === false) {
+        return;
+    }
+
+    let dScore = calculateScore(dealerHand);
+
+    // Dealer hraje, dokud nemá alespoň 17 (standardní pravidlo pro 21)
+    while (dScore < 17) {
+        dealerHand.push(deck.pop());
+        dScore = calculateScore(dealerHand);
+    }
+
+    updateBlackjackUI(false);
+    determineWinner();
+}
+
+function determineWinner() {
+    let pScore = calculateScore(playerHand);
+    let dScore = calculateScore(dealerHand);
+
+    if (dScore > 21) {
+        if (pScore === 21) {
+            updatePoints(15);
+            endGame('Perfektní 21! A dealer navíc přetáhl! Super výhra! 🎉');
+        } else {
+            updatePoints(10);
+            endGame('Dealer měl ' + dScore + ' a přetáhl! Vyhráváš! 💖');
+        }
+    } else if (pScore > dScore) {
+        if (pScore === 21) {
+            updatePoints(15); // Bonus za přesnou 21
+            endGame('Perfektní 21! Máš plný počet bodů! 👑');
+        } else {
+            updatePoints(10); 
+            endGame('Máš ' + pScore + ' vs Dealer ' + dScore + '. Vyhráváš! 💕');
+        }
+    } else if (pScore < dScore) {
+        updatePoints(-5); 
+        endGame('Dealer vyhrává s ' + dScore + ' vs tvých ' + pScore + '. 💔');
+    } else {
+        endGame('Remíza! Oba máte ' + pScore + '. Nikdo neprohrál. 🤝');
+    }
+}
+
+function endGame(resultMessage) {
+    gameActive = false;
+    const messageElement = document.getElementById('message');
+    if (messageElement) {
+        messageElement.innerText = resultMessage;
+    }
+
+    const gameControls = document.getElementById('game-controls');
+    if (gameControls) gameControls.style.display = 'none';
+
+    const startControls = document.getElementById('start-controls');
+    if (startControls) {
+        startControls.style.display = 'flex';
+        const startBtn = startControls.querySelector('button');
+        if (startBtn) startBtn.innerText = "Hrát znovu";
+    }
+}
